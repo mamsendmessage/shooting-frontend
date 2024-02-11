0cbb0a179a2004fdbde517e692f034083352526f
mamsendmessage <133666852+mamsendmessage@users.noreply.github.com>
2024-02-11 22:14:32 +0400

    add css for finished

--- src/app/models/Constants.ts
+++ src/app/models/Constants.ts
@@ -1,7 +1,7 @@
 export class Constants {
-    public static APIAnonymousServerUrl: string = 'http://localhost:2024/anonymous'
-    public static APIServerUrl: string = 'http://localhost:2024/api'
-    public static BaseServerUrl: string = 'http://localhost:2024/'
+    public static APIAnonymousServerUrl: string = 'https://shooting-api.azurewebsites.net/anonymous'
+    public static APIServerUrl: string = 'https://shooting-api.azurewebsites.net/api'
+    public static BaseServerUrl: string = 'https://shooting-api.azurewebsites.net/'
     public static apiWithoutHeader: string[] = [
         '/auth/login'
     ];
